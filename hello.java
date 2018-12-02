f1b9866daa2ee403f0cd4caf87264405060e11fa
Fraser <3538264@myuwc.ac.za>
2018-12-02 23:34:09 +0200

    Add files via upload
    Hello world

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello{
+
+  public static void printHello(){
+      System.out.println("Hello its been a while");
+  }
+  public static void TenHelloStatiments(){
+    for(int i = 0; i<= 10;i++){
+      System.out.println("Hell0");
+    }
+  }
+  public static void main(String [] args){
+    //printHello();
+    TenHelloStatiments();
+    System.out.println("what is going on");
+  }
+}
